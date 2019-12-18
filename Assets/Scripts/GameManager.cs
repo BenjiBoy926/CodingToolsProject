3c70c9fdc6049f74bf480c7fa8ddc393bb187ed7
Codey Huntting <creativecodey@gmail.com>
2019-12-18 14:03:56 -0800

    Some minor changes to the scene, and some scripts added for demo purposes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+
+    private void Update()
+    {
+        if (playerHealth.health <= 0)
+        {
+            // Do a thing!
+        }
+    }
+}
